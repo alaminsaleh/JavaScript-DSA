58cb9e4d8207d795a3fe3561b26d98a124ec9a5f
alaminsaleh <abusalehalamin52@gmail.com>
2026-08-07 13:06:08 +0600

    Completed the Part-II Chapter-08 task-1

--- JavaScript-DSA/Part - II/Chapter-08/practice-1.js
+++ JavaScript-DSA/Part - II/Chapter-08/practice-1.js
@@ -1,1 +1,21 @@
 // Starts the Part-II of JS handbook 
+// Write createBankAccount(initial) that 
+// returns an object with deposit and getBalance functions
+//sharing a private balance via closure.
+
+function createBankAccount(initial) {
+    let balance = initial; //private variable
+
+    return {
+        deposit: function (amount) { // deposit:adds money
+            balance += amount;
+        },
+        getBalance: function () { //getBalance() = shows money
+            return balance;
+        }
+    }
+}
+
+let account = createBankAccount(1000);
+account.deposit(500);
+console.log(account.getBalance());
